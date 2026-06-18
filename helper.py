80c2760a1d4f9813750f064256f4075f9acd3c17
Rozen <rizieqmuhammad.13@gmail.com>
2026-06-18 22:54:48 +0700

    feat; Add helper function to convert text into tokens and vice versa

--- helper.py
+++ helper.py
@@ -1,6 +1,8 @@
 import torch.nn as nn
 import torch
+import tiktoken
 
+from new_GPT_Arch import generate_text_simple
 
 def calculate_params(model:nn.Module):
     """adalah fungsi helper sederhan untuk menghitung banyak parameters"""
@@ -18,3 +20,17 @@ def calculate_size(num_params) -> float:
     else:
         model_in_mb = total_bytes / (1024 * 1024)
         print(f"besar model adalah {model_in_mb:.2f}MB.")
+
+def text_to_token_ids(text, tokenizer):
+    """convert text into tokens"""
+    token = tokenizer.encode(text, allowed_special={"<|endoftext|>"})
+    encoded_tensor = torch.tensor(token).unsqueeze(0)
+    return encoded_tensor
+
+def token_ids_to_text(tokens, tokenizer):
+    """
+    convert the token back into text
+    """
+    text_clean = tokens.squeeze(0)
+    text_decode = tokenizer.decode(text_clean.tolist())
+    return text_decode
