174512823d25a117e5bdb98c3671f22cd47acef4
Rozen <rizieqmuhammad.13@gmail.com>
2026-06-21 17:28:42 +0700

    feat: implement training script for LLM and add visualization function for training losses: - Add function for training calculate batch and dataset losses - implement training_simple function for traininig basic LLMs - Add helper function to visualize training result

--- helper.py
+++ helper.py
@@ -1,8 +1,9 @@
 import torch.nn as nn
 import torch
 import tiktoken
+import matplotlib.pyplot as plt
 
-from new_GPT_Arch import generate_text_simple
+from matplotlib.ticker import MaxNLocator
 
 def calculate_params(model:nn.Module):
     """adalah fungsi helper sederhan untuk menghitung banyak parameters"""
@@ -34,3 +35,24 @@ def token_ids_to_text(tokens, tokenizer):
     text_clean = tokens.squeeze(0)
     text_decode = tokenizer.decode(text_clean.tolist())
     return text_decode
+
+def visualize_training_results(model, training_losses, validation_losses,
+                               tokens_seen, epochs_seen):
+    """
+    Fungsi helper sederhana yang akan digunakan untuk memvisualisasikan 
+    nilai antara
+    """
+    fig, ax1 = plt.subplots(figsize=(5, 3))
+    ax1.plot(epochs_seen, training_losses, label="Training loss")
+    ax1.plot(
+    epochs_seen, validation_losses, linestyle="-.", label="Validation loss"
+    )
+    ax1.set_xlabel("Epochs")
+    ax1.set_ylabel("Loss")
+    ax1.legend(loc="upper right")
+    ax1.xaxis.set_major_locator(MaxNLocator(integer=True))
+    ax2 = ax1.twiny()
+    ax2.plot(tokens_seen, training_losses, alpha=0)
+    ax2.set_xlabel("Tokens seen")
+    fig.tight_layout()
+    plt.show()
--- training.py
+++ training.py
@@ -3,7 +3,11 @@ import tiktoken
 
 from pathlib import Path
 from sampling_data import get_dataloader
-from new_GPT_Arch import GPT2_124M_Config, GPT_Model
+from new_GPT_Arch import GPT2_124M_Config, GPT_Model, generate_text_simple
+from helper import text_to_token_ids, token_ids_to_text, visualize_training_results
+from torch.optim import AdamW
+from tqdm import tqdm
+
 torch.manual_seed(123)
 """
 Tahapan dalam melakukan training
@@ -12,10 +16,11 @@ Tahapan dalam melakukan training
 3. Create the training loop
 4. Text Generation strategies
 """
-
+#CONFIG AND CONSTANST
 dataset_path = Path(r"dataset\the_verdict.txt")
 tokenizer = tiktoken.get_encoding("gpt2")
 model = GPT_Model(GPT2_124M_Config)
+NUM_EPOCHS = 10
 
 with open(dataset_path, mode="r", encoding="utf-8") as f:
     text_data = f.read()
@@ -23,8 +28,6 @@ with open(dataset_path, mode="r", encoding="utf-8") as f:
 #test the length of the dataset
 length_data = len(text_data)
 data_tokens = len(tokenizer.encode(text_data))
-print("banyak data:", length_data)
-print("banyak token:", data_tokens)
 
 train_ratio = 0.9
 split_idx = int(train_ratio * length_data)
@@ -55,10 +58,6 @@ validation_dataloder = get_dataloader(
     drop_last=False,
     num_workers=0
 )
-
-print("train dataloder")
-for x, y in train_dataloader:
-    print(x.shape, y.shape)
 
 def calculate_loss_batch(input_batch, target_batch, model, device):
     """Calculate the loss of a specific batch"""
@@ -95,11 +94,131 @@ def calculate_loss_dataloader(dataloader, model, device, num_batches=None):
     #this mark the total loss of a specifc batches
     return total_loss / num_batches
 
-#testing the dataloader
-device = "cuda" if torch.cuda.is_available() else "cpu"
-model.to(device)
-with torch.no_grad():
-    train_loss = calculate_loss_dataloader(train_dataloader, model, device=device)
-    val_loss = calculate_loss_dataloader(validation_dataloder, model, device=device)
-print(f"Training loss: {train_loss:.2f}")
-print(f"validation loss: {val_loss:.2f}")
+def training_simple(model, train_dataloader, val_dataloaader, 
+                    optimizer, device, num_epochs, eval_freq,
+                    eval_iter, start_context, tokenizer):
+    """
+    This function is for training the model using the model and getting 
+    the output should be the number of loss
+    """
+    train_losses, val_losses, track_token_seen = [], [], []
+    token_seen, global_step = 0, -1
+
+    for epoch in tqdm(range(num_epochs)):
+        model.train()
+        model.to(device)
+        #create idx, and loss
+        for input_batch, target_batch in train_dataloader:
+            optimizer.zero_grad()
+            loss = calculate_loss_batch(
+                input_batch, target_batch,
+                model, device
+            )
+
+            #loss backward
+            loss.backward()
+            #update gradients
+            optimizer.step()
+
+            token_seen += input_batch.numel()
+            global_step +=1
+
+            #this steps is just an optional steps that could've 
+            #been skipped. BUt for education purposes this was added here
+            if global_step % eval_freq == 0:
+                train_loss, val_loss = evaluate_model(
+                    model, train_dataloader, val_dataloaader,
+                    device, eval_iter
+                )
+                train_losses.append(train_loss)
+                val_losses.append(val_loss)
+                track_token_seen.append(token_seen)
+                #print the results ofthis training
+                print(f"Ep {epoch+1} (step: {global_step:06d}):"
+                      f"Train Loss : {train_loss:.2f}, "
+                      f"Val loss: {val_loss:.2f}."
+                      )
+        
+        generate_and_print_sample(model, tokenizer, 
+                                    device, start_context)
+            
+    return train_losses, val_losses, track_token_seen
+
+def evaluate_model(model,train_loader, val_dataloader,
+                   device, eval_iter):
+    """
+    Merupakan fungsi yang digunakan untuk mengevaluasi model
+    biasanya diterapkan setelah melalui proses training
+    """
+    model.eval()
+    with torch.no_grad():
+        train_loss = calculate_loss_dataloader(
+            train_loader, model, device,
+            num_batches=eval_iter
+        )
+
+        val_loss = calculate_loss_dataloader(
+            val_dataloader, model,
+            device, num_batches=eval_iter
+        )
+
+    model.train()
+    return train_loss, val_loss
+
+def generate_and_print_sample(model, tokenizer, device, start_context):
+    """
+    A simple function for generating text
+    """
+    model.eval()
+    context_length = model.pos_emb.weight.shape[0]
+
+    #convert the text into token
+    encoded = text_to_token_ids(
+        start_context, tokenizer=tokenizer
+    ).to(device)
+
+    #generate text
+    with torch.no_grad():
+        output_tokens = generate_text_simple(model, encoded, max_new_tokens=50,
+                             context_size=context_length)
+    #and then you convert it back into text
+    decoded_text = token_ids_to_text(output_tokens, tokenizer=tokenizer)
+    print(f"text yang dirpint adalah: ")
+    print(decoded_text.replace("\n", " "))
+    model.train()
+
+def main():
+    #testing the dataloader
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    model.to(device)
+    with torch.no_grad():
+        train_loss = calculate_loss_dataloader(train_dataloader, model, device=device)
+        val_loss = calculate_loss_dataloader(validation_dataloder, model, device=device)
+    print(f"Training loss: {train_loss:.2f}")
+    print(f"validation loss: {val_loss:.2f}",'\n')
+
+    #---------------------
+    #TRAINING
+    #----------------------
+    print("TRAINING MODEL:")
+    optimizer = AdamW(
+        params=model.parameters(),
+        lr=0.0004, weight_decay=0.1
+    )
+    train_losses, validation_losses, tokens_seen = training_simple(
+        model, train_dataloader=train_dataloader, val_dataloaader=validation_dataloder,
+        optimizer=optimizer, device=device, num_epochs=NUM_EPOCHS,
+        eval_freq = 5, eval_iter=5, start_context="Every effort moves_you", tokenizer=tokenizer
+    )
+
+    epochs_tensor = torch.linspace(0, NUM_EPOCHS, len(train_losses))
+    visualize_training_results(
+        model, train_losses, validation_losses,
+        tokens_seen, epochs_tensor
+    )
+
+
+
+
+if __name__=="__main__":
+    main()
